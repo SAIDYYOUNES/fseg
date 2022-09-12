81b0d2e395dbba4630618f16c1af564618fcc880
SAIDYYOUNES <112939817+SAIDYYOUNES@users.noreply.github.com>
2022-09-12 16:49:44 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,52 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
 int main()
-{ float A,B,R;
-   int o;
-    printf("saisir le premiere nombre :");
-     scanf("%f",&A);
-     printf("saisir le deuxieme nombre:");
-    scanf("%f",&B);
-
-    printf(" pour la somme choisi :1\n");
-
-     printf(" pour la substraction choisi :2\n");
-
-     printf(" pour le produit choisi :3\n");
-
-     printf(" pour la  division choisi :4\n");
-
-    scanf("%d",&o);
-
-
-   switch   (o)
 {
-
- case 1 : R=A+B;
-      printf("%.2f+%.2f=%.2f",A,B,R);
-      break;
-      case 2 : R=A-B;
-      printf("%.2f-%.2f=%.2f",A,B,R);
-      break;
-   case 3 : R=A*B;
-      printf("%.2f*%.2f=%.2f",A,B,R);
-      break;
-  case 4 : R=A/B;
-      printf("%.2f/%.2f=%.2f",A,B,R);
-      break;
+    int x,y,d;
+    printf("entrer le nombre de ligne\n");
+    scanf ("%d",&d);
+    for(x=0;x<=d;x++)
+{
+    for(y=1;y<=d-x;y++)
+    {
+        printf("  ");
 }
 
-
-
-
-
-
-
-
-
-
-
-
+ for(y=1;y<=2*x-1;y++)
+ {
+     printf("* ");
+ }
+printf("\n");
+}
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+

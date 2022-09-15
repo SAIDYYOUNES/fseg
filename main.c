9552951ded24164af328fc441ca70399fb195047
SAIDYYOUNES <112939817+SAIDYYOUNES@users.noreply.github.com>
2022-09-15 10:08:33 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,38 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-
 int main()
 {
-    int x,y,d;
-    printf("entrer le nombre de ligne\n");
-    scanf ("%d",&d);
-    for(x=0;x<=d;x++)
-{
-    for(y=1;y<=d-x;y++)
-    {
-        printf("  ");
-}
+    int S,I,T[10],J,swap,i;
+    printf("donner la taile du tableau:\n");
+    scanf("%d",&S);
+    printf("entrer les %d entiers du tableau:\n",S);
+    for (i=0;i<S;i++)
+    scanf("%d",&T[i]);
+printf("le tableau avant tri\n");
+for (i=0;i<S;i++)
+printf("T[%d]=%d\n",i,T[i]);
 
- for(y=1;y<=2*x-1;y++)
- {
-     printf("* ");
- }
-printf("\n");
+for (J=0;J<S;J++){
+    for (i=J+1;i<S;i++){
+
+if (T[J]>T[i]){
+
+swap=T[J];
+T[J]=T[i];
+T[i]=swap;
+    }
 }
+}
+ printf("le tableau apres tri\n");
+for (I=0;I<S;I++)
+printf("T[%d]=%d\n",I,T[I]);
+
+
     return 0;
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
